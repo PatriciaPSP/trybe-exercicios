bac299476bb6c60ac64ae913a0a93c096d91c6cf
PatriciaPSP <psp_ha@hotmail.com>
2022-04-29 20:02:14 -0300

    Adiciona exe bloco 09

---
+++ exercicios-bloco09/teste.js
@@ -0,0 +1,22 @@
+    // var originalArray = ['Patrícia', 'Lucas', 'Rejane', 'Matheus'];
+    // //use "=" operators
+    // var new1 = originalArray;
+    // //use slice() without parameters can make a "copy"
+    // var new2 = originalArray.slice(-3);
+    // //then three array all are [1,2,3,4,5], let's us do something..
+    // new1.push(100);
+    // //new2.push(111);
+    // //Let's look at the changes in these arrays:
+    // //console.log(new1)
+    // console.log(new2)
+    // //console.log(originalArray)
+
+    const países = {
+      França: 'Paris',
+      Brasil: 'Brasília',
+      Espanha: 'Madrid',
+      Portugal: 'Lisboa',
+    };
+    const pairKeyValue = Object.entries(países);
+    console.log(pairKeyValue);
+    

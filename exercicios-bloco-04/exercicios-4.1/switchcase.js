6d0c57134d5b98f9a4dda56ca4a8d2d2512892cc
PatriciaPSP <psp_ha@hotmail.com>
2022-05-17 10:28:23 -0300

    Adiciona exercícios 4.1

---
+++ exercicios-bloco-04/exercicios-4.1/switchcase.js
@@ -0,0 +1,19 @@
+let trafficLight = 'roxo';
+
+switch (trafficLight) {
+  case 'vermelho':
+    console.log('pare');
+    break;
+
+  case 'amarelo':
+    console.log('atencao');
+    break;
+
+  case 'verde':
+    console.log('siga');
+    break;
+
+  default:
+  console.log('valor não identificado');
+}
+
